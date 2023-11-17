2773ec289e77745cfa4a43777a8b376195b19ca3
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-11-17 13:14:29 +0530

    add answer function added add-answer button event

--- main.js
+++ main.js
@@ -12,7 +12,8 @@ let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({video: true, audio:false})
     document.getElementById('user-1').srcObject = localStream
 }
-let createOffer = async() => {
+
+let createPeerConnection = async (sdpType) =>{
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
@@ -30,10 +31,14 @@ let createOffer = async() => {
 
     peerConnection.onicecandidate = async (event) => {
         if(event.candidate){
-            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
+            document.getElementById(sdpType).value = JSON.stringify(peerConnection.localDescription)
         }
     }
+}
 
+let createOffer = async() => {
+   
+    createPeerConnection('offer-sdp');
     let offer = await peerConnection.createOffer()
     await peerConnection.setLocalDescription(offer)
 
@@ -41,26 +46,7 @@ let createOffer = async() => {
 }
 
 let createAnswer = async() => {
-    peerConnection = new RTCPeerConnection(servers)
-
-    remoteStream = new MediaStream()
-    document.getElementById('user-2').srcObject = remoteStream
-
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track,localStream)
-    })
-
-    peerConnection.ontrack = async (event) => {
-        event.streams[0].getTracks().forEach((track)=>{
-            remoteStream.addTrack(track)
-        })
-    }
-
-    peerConnection.onicecandidate = async (event) => {
-        if(event.candidate){
-            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
-        }
-    }
+    createPeerConnection('answer-sdp')
     let offer = document.getElementById('offer-sdp').value
     if(!offer) return alert('Retrieve offer from peer first...')
     
@@ -72,7 +58,21 @@ let createAnswer = async() => {
 
     document.getElementById('answer-sdp').value = JSON.stringify(answer)
 }
+
+let addAnswer = async ()=>{
+    let answer = document.getElementById('answer-sdp').value
+    if(!answer) return alert('Retrieve answer from peer first...')
+    
+    answer = JSON.parse(answer)
+
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
+    }
+
+}
+
 init()
 
 document.getElementById('create-offer').addEventListener('click', createOffer)
 document.getElementById('create-answer').addEventListener('click', createAnswer)
+document.getElementById('add-answer').addEventListener('click', addAnswer)
